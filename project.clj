d12d52ae57d1479bb0bd115e1d883ef72b664528
Juho Teperi <juho.teperi@iki.fi>
2015-10-04 23:34:54 +0300

    Release 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject deraen/clj-hyphenate "0.1.0-SNAPSHOT"
+(defproject deraen/clj-hyphenate "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
