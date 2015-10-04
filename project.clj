a6d244d82ee3c23932321ed01f06853f8a0a5d4c
Juho Teperi <juho.teperi@iki.fi>
2015-10-04 23:26:07 +0300

    Setup CI

--- project.clj
+++ project.clj
@@ -5,6 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
   :profiles {:dev {:source-paths ["dev-src"]
+                   :plugins [[lein-cloverage "1.0.6"]
+                             [lein-test-out "0.3.1"]]
                    :dependencies [[cheshire "5.5.0"]
                                   [criterium "0.4.3"]]}
              :build {:main clj-hyphenate.build}}
