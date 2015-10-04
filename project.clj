32a1ad8021c8a99c1d72c2f9835993d0b0c210b5
Juho Teperi <juho.teperi@iki.fi>
2015-10-04 23:38:18 +0300

    Fix pom metadata

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject deraen/clj-hyphenate "0.1.0"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "Implementation of Franklin M Ling's hyphnenation algorithm"
+  :url "https://github.com/Deraen/clj-hyphenate"
+  :license {:name "MIT"
+            :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
   :profiles {:dev {:source-paths ["dev-src"]
                    :plugins [[lein-cloverage "1.0.6"]
